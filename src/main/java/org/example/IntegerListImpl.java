ed288c76746741a05d7aec34adec4a35aa694ae0
teach <regeditarivers@gmail.com>
2023-05-26 19:14:32 +0300

    second commit

--- src/main/java/org/example/IntegerListImpl.java
+++ src/main/java/org/example/IntegerListImpl.java
@@ -80,7 +80,9 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public boolean contains(Integer item) {
-        return indexOf(item) != -1;
+        Integer[] storageCopy = toArray();
+        sort(storageCopy);
+        return binarySearch(storageCopy,item);
     }
 
     @Override
@@ -150,5 +152,36 @@ public class IntegerListImpl implements IntegerList {
         if (index < 0 || index > size) {
             throw new InvalidIndexException();
         }
+    }
+
+    public void sort(Integer[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int temp = arr[i];
+            int j = i;
+            while (j > 0 && arr[j - 1] >= temp) {
+                arr[j] = arr[j - 1];
+                j--;
+            }
+            arr[j] = temp;
+        }
+    }
+    private boolean binarySearch(Integer[] arr,Integer item){
+        int min = 0;
+        int max = arr.length - 1;
+
+        while (min <= max) {
+            int mid = (min + max) / 2;
+
+            if (item == arr[mid]) {
+                return true;
+            }
+
+            if (item < arr[mid]) {
+                max = mid - 1;
+            } else {
+                min = mid + 1;
+            }
+        }
+        return false;
     }
 }
